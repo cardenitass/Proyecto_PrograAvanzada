69397e96459c27d6340fb624fab5cd8ebc4ea35b
DanielPPR <dvargas30529@ufide.ac.cr>
2023-02-12 17:30:47 -0600

    Agregar archivos de proyecto.

---
+++ ProyectowebB/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using ProyectowebB.Entities;
+using ProyectowebB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace ProyectowebB.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            UsuarioModel model = new UsuarioModel();
+            
+            return View();
+        }
+        
+    
+    [HttpPost]
+    public ActionResult IniciarSeccion(UsuarioEnt entidad)
+    {
+        UsuarioModel model = new UsuarioModel();
+        model.ValidarUsuario();
+        return View();
+        }
+    }
+
+}
